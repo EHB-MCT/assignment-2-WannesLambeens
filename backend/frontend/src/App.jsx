a8ca5428a7e64f9e580e3d3c416abb5aa240456e
WannesLambeens <113997773+WannesLambeens@users.noreply.github.com>
2025-01-05 22:03:05 +0100

    feat(frontend): configureer basisfrontend

---
+++ backend/frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react';
+import DataList from './components/DataList.jsx';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Mijn Frontend</h1>
+      <p>Welkom bij mijn applicatie!</p>
+    </div>
+  );
+};
+
+
+export default App;
